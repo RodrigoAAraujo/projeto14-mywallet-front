f6a5052fdeec22cf097f114da5d302bfdb14a4f9
RodrigoAAraujo <rodrigo.teth@gmail.com>
2022-11-21 11:42:44 -0300

    url adjusts for deploy

--- src/Settings/urls.js
+++ src/Settings/urls.js
@@ -1,4 +1,4 @@
-export const BackEndServer_SignIn = "http://localhost:5000/sign-in"
-export const BackEndServer_SignUp = "http://localhost:5000/sign-up"
-export const BackEndServer_LogOut = "http://localhost:5000/log-out"
-export const BackEndServer_Wallet = "http://localhost:5000/wallet"
+export const BackEndServer_SignIn = "https://my-wallet-t50r.onrender.com/sign-in"
+export const BackEndServer_SignUp = "https://my-wallet-t50r.onrender.com/sign-up"
+export const BackEndServer_LogOut = "https://my-wallet-t50r.onrender.com/log-out"
+export const BackEndServer_Wallet = "https://my-wallet-t50r.onrender.com/wallet"
